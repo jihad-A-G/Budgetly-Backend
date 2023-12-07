9b113d3b313fa9d8d26f1df2ad4e664aa9bf64a9
mehioitani <muhieddineitani04@gmail.com>
2023-12-07 16:17:10 +0200

    merge

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import * as authController from '../controllers/authController.js'
 
-const router =express.Router();
+const router = express.Router();
 router.post('/signup',authController.userSignUp);
 router.post('/login', authController.userLogin);
 router.post('/resetPassword',authController.requestResetPassword);
