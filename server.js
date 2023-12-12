0ac8b6437c4cab165f9f335297b8be4d32ed7a51
anitta-sleiman <sleiman3198@gmail.com>
2023-12-12 15:47:50 +0200

    N/A

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 import sequelize from "./db.js";
 import bodyParser from "body-parser";
-import './associations.js';
-// import verfiyToken from "./authenticate.js";
-import cors from 'cors';
+import "./associations.js";
+import verfiyToken from "./authenticate.js";
+import cors from "cors";
 import { Server } from "socket.io";
 import { createServer } from "http";
 //Routers
@@ -17,42 +17,42 @@ import authRouter from "./routes/authRouter.js";
 import categoryRouter from "./routes/categoryRouter.js";
 import incomeRouter from "./routes/incomeRouter.js";
 import goalRouter from "./routes/goalRouter.js";
-import expenseRouter from './routes/expenseRouter.js';
+import expenseRouter from "./routes/expenseRouter.js";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use('/images',express.static("images"));
-
+app.use("/images", express.static("images"));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 app.use("/api/auth", authRouter);
-// app.use(verfiyToken);
+app.use(verfiyToken);
 app.use("/api/company", companyRouter);
 app.use("/api/admin", AdminRouter);
 app.use("/api/user", userRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/income", incomeRouter);
 app.use("/api/goal", goalRouter);
-app.use('/api',expenseRouter);
+app.use("/api", expenseRouter);
 
-sequelize.sync({ force:false });
+sequelize.sync({ force: false});
 const httpServer = createServer(app);
- const io = new Server(httpServer, { 
-  cors:{
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST"],
-  }});
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 io.on("connection", (socket) => {
   console.log(socket.id);
-  socket.on('joinAdminRoom',(user)=>{
-    if(user.role==='admin'){
-      socket.join('adminRoom');
+  socket.on("joinAdminRoom", (user) => {
+    if (user.role === "admin") {
+      socket.join("adminRoom");
     }
-  })
+  });
 });
 httpServer.listen(process.env.PORT);
 
